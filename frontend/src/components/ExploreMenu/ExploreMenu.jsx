14e2abb59b3a7c9fe0d7b7d682f9999447cd2d41
Denish Murawala <murawaladenish@gmail.com>
2026-02-02 15:51:20 +0530

    created explore-menu component

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import './ExploreMenu.css'
-import { menu_list } from '../../assets/assets'
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
 
 const ExploreMenu = () => {
   return (
-    <div>
-      
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Choose from a diverse menu featuring a delectable array of dishes. Our
+        mission is to bring you the best food and taste experience, anytime,
+        anywhere.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item) => (
+          <div key={item.menu_name} className="explore-menu-list-item">
+            <img src={item.menu_image} alt={item.menu_name} />
+            <p>{item.menu_name}</p>
+          </div>
+        ))}
+      </div>
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default ExploreMenu
+export default ExploreMenu;
