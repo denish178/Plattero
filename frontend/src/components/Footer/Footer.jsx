dc10376175ec7e55b8656fdba6ac158ee02b4926
Denish Murawala <murawaladenish@gmail.com>
2026-02-11 19:42:39 +0530

    adjusted responsiveness of footer on small screens

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,39 +4,44 @@ import { assets } from "../../assets/assets";
 
 const Footer = () => {
   return (
-    <div className="footer" id="footer">
+    <footer className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-          <img src={assets.logo} alt="" />
+          <img src={assets.logo} alt="Plattero logo" />
           <p>© 2022 Plattero</p>
+
           <div className="foot-social-icons">
-            <img src={assets.facebook_icon} alt="" />
-            <img src={assets.twitter_icon} alt="" />
-            <img src={assets.linkedin_icon} alt="" />
+            <a href="#"><img src={assets.facebook_icon} alt="Facebook" /></a>
+            <a href="#"><img src={assets.twitter_icon} alt="Twitter" /></a>
+            <a href="#"><img src={assets.linkedin_icon} alt="LinkedIn" /></a>
           </div>
         </div>
+
         <div className="footer-content-center">
           <h2>COMPANY</h2>
           <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Delivery</li>
-            <li>Privacy policy</li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Delivery</a></li>
+            <li><a href="#">Privacy policy</a></li>
           </ul>
         </div>
+
         <div className="footer-content-right">
           <h2>GET IN TOUCH</h2>
           <ul>
-            <li>+91-8401275343</li>
-            <li>murawaladenish@gmail.com</li>
+            <li><a href="tel:+918401275343">+91-8401275343</a></li>
+            <li><a href="mailto:murawaladenish@gmail.com">murawaladenish@gmail.com</a></li>
           </ul>
         </div>
       </div>
+
       <hr />
+
       <p className="footer-copyright">
-        Copyright 2026 &copy; Plattero.com - All Right Reserved.
+        Copyright 2026 &copy; Plattero.com - All Rights Reserved.
       </p>
-    </div>
+    </footer>
   );
 };
 
